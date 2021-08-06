fedf92b016bfe11fd7b55301b51b58573d50d62b
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-05 21:14:08 -0300

    Adicionado babel no projeto

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _user$address;
+
+var user = {
+  name: 'Roberto',
+};
+console.log(
+  (_user$address = user.address) === null || _user$address === void 0
+    ? void 0
+    : _user$address.street
+);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const user = {
+  name: 'Roberto',
+};
+
+console.log(user.address?.street);
