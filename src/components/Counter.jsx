f9f13ec99a6d9d30e03052312a88c78bc6bf9fb3
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-07 14:48:24 -0300

    Feito Contador para testar

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [contador, setContador] = useState(0);
+
+
+  function increment() {
+    setContador(contador+1);
+  }
+
+  return (
+    <div>
+      <h2>{contador}</h2>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+    </div>
+  )
+}
