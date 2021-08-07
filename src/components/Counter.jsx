8261b5988bf10f386d78ac91cfebc249254d89c6
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-07 18:53:06 -0300

    Adicionado webpack-refresh-plugin

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,7 +12,7 @@ export function Counter() {
     <div>
       <h2>{contador}</h2>
       <button type="button" onClick={increment}>
-        Increment
+        Increment = {contador}
       </button>
     </div>
   )
