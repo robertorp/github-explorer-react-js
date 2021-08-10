6d336a04f57d7b74f5ce8fd7dd9c6faf8f895319
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-09 21:35:45 -0300

    Estilisado repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 const repository = {
   name: 'unform',
@@ -6,7 +8,12 @@ const repository = {
   link: 'https://github.com/unform/unform'
 };
 
+//https://api.github.com/users/robertorp/repos
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
