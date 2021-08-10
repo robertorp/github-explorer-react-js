2a6a3e8135dd04640cba496884d5d0c81bc11033
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-08-10 00:07:25 -0300

    Adicionado typagem

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useState, useEffect } from "react";
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/users/robertorp/repos')
@@ -17,7 +23,7 @@ export function RepositoryList() {
 
       <ul>
        {repositories.map(repositorio => {
-         return <RepositoryItem key={repositorio.name} repository={repositorio}></RepositoryItem>;
+         return <RepositoryItem key={repositorio.name} repository={repositorio}/>;
        })}
       </ul>
     </section>
